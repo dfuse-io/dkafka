32fc2fd7df9edfeb570c876891e3cfcd4d906aa7
Stéphane Duchesneau <stephane.duchesneau@eoscanada.com>
2020-12-10 13:39:43 -0500

    more work, gives interesting output with CEL templating/conditions, unique ID, etc.

---
+++ cel.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/dfuse-io/dfuse-eosio/filtering"
+	"github.com/google/cel-go/cel"
+	"github.com/google/cel-go/checker/decls"
+)
+
+func exprToCelProgram(stripped string) (prog cel.Program, err error) {
+	env, err := cel.NewEnv(filtering.ActionTraceDeclarations)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	exprAst, issues := env.Compile(stripped)
+	if issues != nil && issues.Err() != nil {
+		fmt.Println(fmt.Errorf("parse filter: %w", issues.Err()))
+		return
+	}
+	fmt.Println(exprAst.ResultType().GetType())
+	fmt.Println(decls.NewListType(decls.String).GetType())
+
+	prog, err = env.Program(exprAst)
+	if err != nil {
+		fmt.Println(fmt.Errorf("program: %w", err))
+		return
+	}
+
+	return
+}
--- main.go
+++ main.go
@@ -2,14 +2,19 @@ package main
 
 import (
 	"context"
+	"crypto/sha256"
 	"crypto/tls"
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
+	"reflect"
 	"strings"
 
-	eosio "github.com/dfuse-io/dkafka/pb/eosio-codec"
+	"github.com/dfuse-io/dfuse-eosio/filtering"
+	pbcodec "github.com/dfuse-io/dfuse-eosio/pb/dfuse/eosio/codec/v1"
 	pbbstream "github.com/dfuse-io/pbgo/dfuse/bstream/v1"
 	"github.com/golang/protobuf/ptypes"
+	"github.com/google/cel-go/cel"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
 
@@ -26,11 +31,29 @@ var grpcEndpoint = "localhost:13035"
 var dfuseToken = "disabled"
 var topic = "quickstart-events"
 var kafkaEndpoints = []string{"127.0.0.1:9092"}
-var eventType = "dfuse.action_trace"
-var eventSource = "dfuse.chain"
+var eventSource = "dfuse.chain.tests"
+
+var eventKeyExpr = "auth.filter(a, a.contains('@'))"
+var eventTypeExpr = "(notif?'!':'')+account+'/'+action" // account::action or   'notify'::account::action
+var startBlockNum = int64(16)
+var stopBlockNum = uint64(18)
+
+type extension struct {
+	name string
+	expr string
+	prog cel.Program
+}
+
+var extensions = []*extension{
+	{"concerned", "has(data.account)?data.account:'blah'", nil},
+	//{"blkstep", "step", nil},
+}
+
+var irreversibleOnly = false
+
+var includeFilter = "action != 'setabi'"
 
 func main() {
-
 	saramaConfig := sarama.NewConfig()
 	saramaConfig.Version = sarama.V2_0_0_0
 
@@ -53,12 +76,12 @@ func main() {
 	if plaintext {
 		dialOptions = append(dialOptions, grpc.WithInsecure())
 	} else {
-		//		credential := oauth.NewOauthAccess(&oauth2.Token{AccessToken: dfuseToken, TokenType: "Bearer"})
 		transportCreds := credentials.NewTLS(&tls.Config{
 			InsecureSkipVerify: true,
 		})
-		//grpc.WithPerRPCCredentials(credential),
 		dialOptions = append(dialOptions, grpc.WithTransportCredentials(transportCreds))
+		// credential := oauth.NewOauthAccess(&oauth2.Token{AccessToken: dfuseToken, TokenType: "Bearer"})
+		// dialOptions  = append(dialOptions, grpc.WithPerRPCCredentials(credential))
 		fmt.Println("connecting to conn with", addr, dialOptions)
 	}
 	conn, err := grpc.Dial(addr,
@@ -71,21 +94,41 @@ func main() {
 	client := pbbstream.NewBlockStreamV2Client(conn)
 
 	req := &pbbstream.BlocksRequestV2{
-		StartBlockNum:     10,
-		StopBlockNum:      100,
+		StartBlockNum:     startBlockNum,
+		StopBlockNum:      stopBlockNum,
 		ExcludeStartBlock: false,
 		Decoded:           true,
 		HandleForks:       true,
-		HandleForksSteps: []pbbstream.ForkStep{
-			pbbstream.ForkStep_STEP_IRREVERSIBLE,
-		},
-		IncludeFilterExpr: "action != 'blah'",
+		IncludeFilterExpr: includeFilter,
+	}
+	if irreversibleOnly {
+		req.HandleForksSteps = []pbbstream.ForkStep{pbbstream.ForkStep_STEP_IRREVERSIBLE}
 	}
 
 	ctx := context.Background()
 	executor, err := client.Blocks(ctx, req)
 	if err != nil {
 		panic(err)
+	}
+
+	eventTypeProg, err := exprToCelProgram(eventTypeExpr)
+	if err != nil {
+		fmt.Println("error err", err)
+		return
+	}
+	eventKeyProg, err := exprToCelProgram(eventKeyExpr)
+	if err != nil {
+		fmt.Println("error err", err)
+		return
+	}
+
+	for _, ext := range extensions {
+		prog, err := exprToCelProgram(ext.expr)
+		if err != nil {
+			fmt.Println("error err", err)
+			return
+		}
+		ext.prog = prog
 	}
 
 	for {
@@ -95,49 +138,97 @@ func main() {
 			return
 		}
 
-		blk := &eosio.Block{}
+		blk := &pbcodec.Block{}
 		if err := ptypes.UnmarshalAny(msg.Block, blk); err != nil {
 			fmt.Println(fmt.Errorf("decoding any of type %q: %w", msg.Block.TypeUrl, err))
 			return
 		}
-		log.Printf("getting block %d\n", blk.Number)
+
+		step := sanitizeStep(msg.Step.String())
 
 		for _, trx := range blk.FilteredTransactionTraces {
+			status := sanitizeStatus(trx.Receipt.Status.String())
+			memoizableTrxTrace := filtering.MemoizableTrxTrace{TrxTrace: trx}
 			for _, act := range trx.ActionTraces {
-				fmt.Println("in trxaction")
 				if !act.FilteringMatched {
-					fmt.Println("skipping")
 					continue
 				}
-				fmt.Println("not skipping")
 				var jsonData json.RawMessage
 				if act.Action.JsonData != "" {
 					jsonData = json.RawMessage(act.Action.JsonData)
 				}
+				activation := filtering.NewActionTraceActivation(
+					act,
+					memoizableTrxTrace,
+					msg.Step.String(),
+				)
+
+				var auths []string
+				for _, auth := range act.Action.Authorization {
+					auths = append(auths, auth.Authorization())
+				}
+
 				eosioAction := event{
 					BlockNum:      blk.Number,
 					BlockID:       blk.Id,
+					Status:        status,
+					Step:          step,
 					TransactionID: trx.Id,
-					Account:       act.Account(),
-					Receiver:      act.Receiver,
-					ActionName:    act.Name(),
-					JSONData:      &jsonData,
+					ActionInfo: ActionInfo{
+						Account:        act.Account(),
+						Receiver:       act.Receiver,
+						Action:         act.Name(),
+						JSONData:       &jsonData,
+						Authorization:  auths,
+						GlobalSequence: act.Receipt.GlobalSequence,
+					},
 				}
-				e := cloudevents.NewEvent()
-				e.SetID(fmt.Sprintf("%s-%s-%d", blk.Id, trx.Id, act.ActionOrdinal))
-				e.SetType(eventType)
-				e.SetSource(eventSource)
-				e.SetExtension("datacontenttype", "application/json")
-				e.SetTime(blk.MustTime())
-				_ = e.SetData(cloudevents.ApplicationJSON, eosioAction)
 
-				if result := c.Send(
-					kafka_sarama.WithMessageKey(context.Background(), sarama.StringEncoder(e.ID())),
-					e,
-				); cloudevents.IsUndelivered(result) {
-					log.Printf("failed to send: %v", err)
-				} else {
-					log.Printf("sent: %d, accepted: %t", blk.Number, cloudevents.IsACK(result))
+				eventType, err := evalString(eventTypeProg, activation)
+				if err != nil {
+					fmt.Println("error eventtype eval", err)
+					return
+				}
+
+				extensionsKV := make(map[string]string)
+				for _, ext := range extensions {
+					val, err := evalString(ext.prog, activation)
+					if err != nil {
+						fmt.Println(fmt.Errorf("program: %w", err))
+						return
+					}
+					extensionsKV[ext.name] = val
+
+				}
+
+				eventKeys, err := evalStringArray(eventKeyProg, activation)
+				if err != nil {
+					fmt.Println("error eventkeyeval", err)
+					return
+				}
+
+				for _, eventKey := range eventKeys {
+					e := cloudevents.NewEvent()
+					e.SetID(hashString(fmt.Sprintf("%s%s%d%s%s", blk.Id, trx.Id, act.ExecutionIndex, msg.Step.String(), eventKey)))
+					e.SetType(eventType)
+					e.SetSource(eventSource)
+					for k, v := range extensionsKV {
+						e.SetExtension(k, v)
+					}
+					e.SetExtension("datacontenttype", "application/json")
+					e.SetExtension("blkstep", step)
+
+					e.SetTime(blk.MustTime())
+					_ = e.SetData(cloudevents.ApplicationJSON, eosioAction)
+
+					if result := c.Send(
+						kafka_sarama.WithMessageKey(ctx, sarama.StringEncoder(eventKey)),
+						e,
+					); cloudevents.IsUndelivered(result) {
+						log.Printf("failed to send: %v", err)
+					} else {
+						log.Printf("sent: blknum %d, id: %s, extensions: %v", blk.Number, e.ID(), e.Extensions())
+					}
 				}
 
 			}
@@ -145,18 +236,66 @@ func main() {
 	}
 }
 
+type ActionInfo struct {
+	Account        string           `json:"account"`
+	Receiver       string           `json:"receiver"`
+	Action         string           `json:"action"`
+	GlobalSequence uint64           `json:"global_seq"`
+	Authorization  []string         `json:"authorizations"`
+	JSONData       *json.RawMessage `json:"json_data"`
+}
+
 type event struct {
-	BlockNum      uint32           `json:"block_num"`
-	BlockID       string           `json:"block_id"`
-	TransactionID string           `json:"transaction_id"`
-	Receiver      string           `json:"receiver"`
-	Account       string           `json:"account"`
-	ActionName    string           `json:"action_name"`
-	JSONData      *json.RawMessage `json:"json_data"`
+	BlockNum      uint32     `json:"block_num"`
+	BlockID       string     `json:"block_id"`
+	Status        string     `json:"status"`
+	Step          string     `json:"block_step"`
+	TransactionID string     `json:"trx_id"`
+	ActionInfo    ActionInfo `json:"act_info"`
 }
 
 func (e event) JSON() []byte {
 	b, _ := json.Marshal(e)
 	return b
 
+}
+
+func hashString(data string) string {
+	h := sha256.New()
+	h.Write([]byte(data))
+	return base64.StdEncoding.EncodeToString(([]byte(h.Sum(nil))))
+}
+
+var stringType = reflect.TypeOf("")
+var stringArrayType = reflect.TypeOf([]string{})
+
+func evalString(prog cel.Program, activation interface{}) (string, error) {
+	res, _, err := prog.Eval(activation)
+	if err != nil {
+		return "", err
+	}
+	out, err := res.ConvertToNative(stringType)
+	if err != nil {
+		return "", err
+	}
+	return out.(string), nil
+}
+
+func evalStringArray(prog cel.Program, activation interface{}) ([]string, error) {
+	res, _, err := prog.Eval(activation)
+	if err != nil {
+		return nil, err
+	}
+	out, err := res.ConvertToNative(stringArrayType)
+	if err != nil {
+		return nil, err
+	}
+	return out.([]string), nil
+}
+
+func sanitizeStep(step string) string {
+	return strings.Title(strings.TrimPrefix(step, "STEP_"))
+}
+func sanitizeStatus(status string) string {
+	return strings.Title(strings.TrimPrefix(status, "TRANSACTIONSTATUS_"))
 }
