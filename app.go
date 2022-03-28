585f522ce2b3a9a39eef08a44de552e810425944
Christophe Opoix <christophe.opoix@ultra.io>
2022-03-28 17:30:52 +0200

    reinclude inject action in cdc table mode

--- app.go
+++ app.go
@@ -454,7 +454,7 @@ func createCdcKeyExpressions(cdcExpression string, env cel.EnvOption) (cdcProgra
 }
 
 func createCdCFilter(account string, executed bool) string {
-	filter := fmt.Sprintf("account==\"%s\" && receiver==\"%s\" && action!=\"inject\" && action!=\"fixaccount\"", account, account)
+	filter := fmt.Sprintf("account==\"%s\" && receiver==\"%s\" && action!=\"fixaccount\"", account, account)
 	if executed {
 		filter = fmt.Sprintf("executed && %s", filter)
 	}
